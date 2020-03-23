fae5f42daf0d1d8468346aa0d0d4ee5c830bfd4a
Michael Reel <mykilr+github@gmail.com>
2020-03-23 19:55:10 +0000

    Single inaccurate high-pitched(!) tone generated

--- Control.gd
+++ Control.gd
@@ -1,8 +1,33 @@
 extends Control
 
-onready var playback := $AudioStreamPlayer
+var hz : float = 440
+var pulse_hz : float = 44100
+var phase : float = 0
+var playback : AudioStreamPlayback
+
+onready var player := $AudioStreamPlayer
+onready var playing := false
 
 func _on_CheckBox_toggled(button_pressed : bool):
 	print("On " + str(button_pressed))
+	playback = player.get_stream_playback()
+	playing = button_pressed
+	if playing:
+		_fill_buffer()
+		player.play()
+	else:
+		player.stop()
+
+func _process(delta : float):
+	if playing:
+		_fill_buffer()
 	
-	
+func _fill_buffer():
+	var increment = (1.0 / (hz / pulse_hz))
+	var to_fill = playback.get_frames_available()
+	while to_fill > 0:
+		playback.push_frame(
+			Vector2(1.0,1.0) * sin(phase * (PI * 2.0))
+		)
+		phase = fmod((phase + increment), 1.0)
+		to_fill -= 1
