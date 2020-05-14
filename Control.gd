925211de284317567150980c441aa3fa586e00cf
Michael Reel <mykilr@gmail.com>
2020-05-14 17:56:15 +0100

    Added a couple of simple controls and scaled the UI up to fisher-price level.

--- Control.gd
+++ Control.gd
@@ -1,7 +1,12 @@
 extends Control
 
+
+
 onready var player := $AudioStreamPlayer
 onready var playing := false
+onready var noiseBusIndex : int = AudioServer.get_bus_index("Noise Bus")
+onready var filterIndex : int = 0
+onready var lowPass : AudioEffectLowPassFilter = AudioServer.get_bus_effect(noiseBusIndex, filterIndex)
 
 func _on_CheckBox_toggled(button_pressed : bool):
 	print("On " + str(button_pressed))
@@ -10,3 +15,9 @@ func _on_CheckBox_toggled(button_pressed : bool):
 		player.play()
 	else:
 		player.stop()
+
+func _on_ClipSelect_value_changed(value : float):
+	lowPass.cutoff_hz = value
+
+func _on_Volume_value_changed(value : float):
+	AudioServer.set_bus_volume_db(noiseBusIndex, value)
